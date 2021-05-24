69172eb07fe0bb41aca69e6e46932249719ce30d
Kiyeon Cho <qsky8520@gmail.com>
2021-05-24 19:01:46 +0900

    커서 이동&변경 함수 추가

--- Term project_Tetris/Term project_Tetris.cpp
+++ Term project_Tetris/Term project_Tetris.cpp
@@ -19,3 +19,30 @@
 #define EMPTY 0
 #define WALL 1
 #define INACTIVE_BLOCK (block.type+11)
+
+void gotoxy(int x, int y) {
+	COORD pos = { 2 * x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+typedef enum { NOCURSOR, SOLIDCURSOR, NORMALCULSOR } CURSOR_TYPE;	// 열거형 정의
+void setcursortype(CURSOR_TYPE c) {
+	CONSOLE_CURSOR_INFO CurInfo;	// typedef를 사용하여 enum 생략
+
+	switch (c) {
+	case NOCURSOR:
+		CurInfo.dwSize = 1;
+		CurInfo.bVisible = FALSE;
+		break;
+	case SOLIDCURSOR:
+		CurInfo.dwSize = 100;
+		CurInfo.bVisible = TRUE;
+		break;
+	case NORMALCULSOR:
+		CurInfo.dwSize = 20;
+		CurInfo.bVisible = TRUE;
+		break;
+	}
+
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &CurInfo);
+}
