bdf960abefe62e8cdfb60fbc87a10f5e0122802f
Kiyeon Cho <qsky8520@gmail.com>
2021-05-24 19:00:50 +0900

    setting_Default value

--- Term project_Tetris/Term project_Tetris.cpp
+++ Term project_Tetris/Term project_Tetris.cpp
@@ -1,1 +1,21 @@
-﻿
+﻿#include<stdio.h>
+#include<windows.h>
+#include<conio.h>
+#include<time.h>
+#include<stdlib.h>
+
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
+
+#define SINGLE 0
+#define PLAYER1 1
+#define PLAYER2 2
+#define COMPUTER 3
+
+#define ACTIVE_BLOCK ((block.type+11)*(-1))
+#define CEILLING -1
+#define EMPTY 0
+#define WALL 1
+#define INACTIVE_BLOCK (block.type+11)
